079fb0f54cdf01149b13a98f2e86f49dd81dc435
kentan <hyperkentakun@gmail.com>
2014-07-20 00:07:52 -0600

    changed package reference.

--- SmartBirdRestfulServer/src/main/org/smartbirdpj/test/SBMessageFileDaoTest.java
+++ SmartBirdRestfulServer/src/main/org/smartbirdpj/test/SBMessageFileDaoTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.sb.mdl.enm.TileEnum;
+import org.smartbirdpj.mdl.enm.TileEnum;
 
 import org.smartbirdpj.dao.SBMessageFileDao;
 import org.smartbirdpj.message.SBMessage;
