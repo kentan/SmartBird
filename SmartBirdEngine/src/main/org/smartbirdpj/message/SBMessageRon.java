c00863d122d0bdda981911f6ab83d3c02ae91d44
kentan <hyperkentakun@gmail.com>
2014-08-20 22:18:20 -0600

    Fixed the bugs about message at ron.

--- SmartBirdEngine/src/main/org/smartbirdpj/message/SBMessageRon.java
+++ SmartBirdEngine/src/main/org/smartbirdpj/message/SBMessageRon.java
@@ -11,13 +11,12 @@ public class SBMessageRon extends SBMessage {
 	public SBMessageRon(int playerId, TileEnum ronTile,PaidPoint point){
 		super(playerId, OPERATION);
 
-//		Map<String,Object> jsonChildMap = new HashMap<String, Object>();
+
 		this.jsonValueMap.put(JSON_KEY_RON_TILE,ronTile.toString());
-		this.jsonValueMap.put(JSON_KEY_RONNED_PLAYER,ronTile.toString());
 		int paying = point.getPayingPlayerIdOnPoint1().get(0);
 		int p= point.getPoint1();		
 
-		this.jsonValueMap.put("player" + String.valueOf(point.getPaidPlayerId()) + "payment",p);
+		this.jsonValueMap.put("player" + paying + "payment",p);
 		this.jsonValueMap.put(JSON_KEY_RONNED_PLAYER,paying);
 	}
 }
